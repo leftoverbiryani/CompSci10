4ce0498dc99548fbbc3a831cf326cf854e2ee149
Sanay <sanay0627@gmail.com>
2019-11-06 10:43:06 -0500

    9a commit

--- 9aDaptardar/Main.py
+++ 9aDaptardar/Main.py
@@ -1,6 +1,6 @@
 #Sanay Daptardar, CompSci 10, Lab 9a
-#Extra:
-def flVowel(x):
+#Extra: part 6 prints something else if there is only one aplus.
+def flVowel(x): #creating methods COMMENT 1
      newx = x.lower()
      c = newx[0]
      rc = newx[len(x) - 1]
@@ -18,6 +18,76 @@ def flVowel(x):
 def nameFlipper(x):
     c = x.find(" ")
     print("{}, {}" .format((x[c:len(x)]), x[0:c]))
+    print()
+
+def avgLen(x, y):
+    c = len(x)
+    c1 = len(y)
+    avg = (c + c1) / 2
+    print("Average length: {:0.1f}" .format(avg))
+    print()
+
+def repeats(x):
+    c = x[0]
+    c1 = x.rfind(c)
+    if(c1 == 0):
+        print("Repeats: NO")
+    else:
+        print("Repeats: YES")
+    print()
+
+def changeCase(x, y):
+    if(y):
+        print("Changed case: {}" .format(x.upper()))
+    else:
+        print(("Changed case: {}" .format(x.lower())))
+    print()
+
+def moreAplus(x): #part 6, extra:
+    c = x.count("aplus") #counting aplus's in string COMMENT 2
+    if(c > 1):
+        print("More than 1 aplus in {}" .format(x))
+    elif(c == 0):
+        print("No aplus in {}!" .format(x))
+    elif(c == 1):
+        print("Only 1 aplus in {}" .format(x))
+    print()
+
+def validSocial(x):
+    c = x[3]
+    c1 = x[6]
+    ct = "-"
+    cf = x[7:12]
+    if(c == ct and c1 == ct):
+        print("Valid, {}" .format(cf))
+    else:
+        print("{} is invalid" .format(x))
+    print()
+
+def pairCount(x):
+    count = 0
+    i = len(x) - 1
+    while(i > 0):
+        c = x[i]
+        c1 = x[i-1];
+        if(c == c1):
+            count += 1
+        i -= 1
+    print("Pairs in {}: {}" .format(x, count))
+    print()
+
+def ceaserCipher(x, y):
+    cv = 0
+    alph = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    al = len(alph)
+    for char in x:
+        for i in range(al):
+            if(char == alph[i]): #comparing char to alphabet COMMENT 3
+                cv = i - y
+                if (cv < 0):
+                    cv += 26
+                print(alph[cv], end='')
+    print()
 #Main
 flVowel("Car")
 flVowel("Appalachian")
@@ -26,4 +96,24 @@ flVowel("Orange")
 
 nameFlipper("Sanay Daptardar")
 
+avgLen("dog", "cats")
+avgLen("Pneumonia", "Bruh")
+
+repeats("dogsbruheddogs")
+repeats("xylophone")
+
+changeCase("BruHbRuH", False) #calling methods COMMENT 4
+changeCase("BruHbRuH", True)
+
+moreAplus("Bruhaplusbruhaplus")
+moreAplus("Bruhaplusbruh")
+moreAplus("Bruh")
+
+validSocial("463-44-5678")
+validSocial("44-144-5678")
+
+pairCount("bbruhhiissthecoollesst")
+pairCount("Mississippi")
+
+ceaserCipher("how do you do", 4)
 
