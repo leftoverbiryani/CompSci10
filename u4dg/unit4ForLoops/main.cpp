1328277d057be4ff97e35c2fe0675def08de8a83
Sanay <sanay0627@gmail.com>
2019-12-16 11:24:50 -0500

    Unit 4 For Loops commit 1

--- u4dg/unit4ForLoops/main.cpp
+++ u4dg/unit4ForLoops/main.cpp
@@ -8,7 +8,8 @@ Extra:
 using namespace std;
 
 int main() {
-    //part 1
+
+    /*part 1
     for(int i = 0; i <= 5; i++)
     {
         if(i != 5)
@@ -16,6 +17,39 @@ int main() {
         else if(i == 5)
             cout << "C++ knows when to stop." << endl;
     }
+     */
 
+    /*part 2
+
+    double p2num;
+    double p2avg = 0;
+
+    for(int i = 1; i <= 5; i++){
+        cout << "Enter number " << i << ": " << endl;
+        cin >> p2num;
+        p2avg += p2num;
+    }
+    cout << "Total: " << p2avg << endl;
+    p2avg /= 5;
+    cout << "Average: " << p2avg << endl;
+    */
+
+    /*part 3
+
+    for(int i = 0; i < 8; i++)
+    {
+        cout << pow(2, i) << " ";
+    }
+        cout << "\n";
+    */
+
+    //part 4
+    char c [] = "LIVERPOOL";
+
+    for(int i = sizeof(c); i >= 0; i--)
+    {
+        cout << c[i];
+    }
+    cout << "\n";
     return 0;
 }
