eba6e74687e074528513d8c65587b19ddcb5df05
Sanay <sanay0627@gmail.com>
2019-12-18 10:19:41 -0500

    looping quiz commit

---
+++ u4dg/loopingQuiz/cmake-build-debug/CMakeFiles/loopingQuiz.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/loopingQuiz.dir/main.cpp.obj"
+  "loopingQuiz.exe"
+  "loopingQuiz.exe.manifest"
+  "loopingQuiz.lib"
+  "loopingQuiz.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/loopingQuiz.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ u4dg/loopingQuiz/main.cpp
@@ -0,0 +1,19 @@
+/*
+ Sanay Daptardar. CompSci 10A, Unit 4 looping quiz
+ */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    //part 1
+    for(int i = 0; i < 10; i++)
+        cout << "C++ Rules!" << endl;
+    cout << endl;
+
+    //part 2
+    for(int i = 100; i >= 1; i--)
+        cout << i << endl;
+    cout << endl;
+    return 0;
+}
